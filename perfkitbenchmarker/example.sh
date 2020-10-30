7adfbb4b0dca9757dd4eaf48b8b8967f094100c4
Liam Young <liam.young@canonical.com>
2020-10-30 11:46:54 +0000

    Fix hardcoded path

--- perfkitbenchmarker/example.sh
+++ perfkitbenchmarker/example.sh
@@ -5,4 +5,4 @@
 source perfkit_venv
 source ../openrc
 
-pkb.py --cloud=OpenStack --machine_type=m1.small --benchmark_config_file=/home/ubuntu/bugs/openstack-perf-testing/perfkitbenchmarker/configs/os.yaml --openstack_network=private --benchmarks=iperf --os_type=ubuntu2004 --openstack_floating_ip_pool=ext_net --http_proxy=$vm_http_proxy --https_proxy=$vm_https_proxy
+pkb.py --cloud=OpenStack --machine_type=m1.small --benchmark_config_file=configs/os.yaml --openstack_network=private --benchmarks=iperf --os_type=ubuntu2004 --openstack_floating_ip_pool=ext_net --http_proxy=$vm_http_proxy --https_proxy=$vm_https_proxy
